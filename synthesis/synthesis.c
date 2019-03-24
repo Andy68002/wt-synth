01c4c1ab7399486250ac541f201d974a1128b9a0
Andy68002 <andrew@andrewmclaurin.com>
2019-03-24 19:49:32 -0400

    Linked time library, changed from realtime to monotonic

--- synthesis/synthesis.c
+++ synthesis/synthesis.c
@@ -5,6 +5,7 @@ Author:
   Andrew McLaurin
   24 March 2019
 */
+#define _POSIX_C_SOURCE 199309L
 #include <stdio.h>
 #include <time.h>
 #include "synthesis.h"
@@ -14,8 +15,9 @@ void DDS(int* note_tuning_words, int* note_attenuation, int* note_enable) {
 	long time_change;
 	struct timespec current_time;
 	char beat_10ms_timer = 0;
-
-	clock_gettime(CLOCK_REALTIME, &current_time);
+  char beat_100_ms_timer = 0;
+  while(1) {
+	clock_gettime(CLOCK_MONOTONIC, &current_time);
 	last_beat = current_time.tv_nsec;
 
 	//timer
@@ -23,13 +25,21 @@ void DDS(int* note_tuning_words, int* note_attenuation, int* note_enable) {
 	time_change = current_time.tv_nsec - last_beat;
 	if (time_change < 0) time_change += 1000000000;
 	if (time_change > 1000000) {
+      printf("made it to 10ms");
 		last_beat += 1000000;
 		if (last_beat > 1000000000) last_beat -=1000000000;
 
 		beat_10ms_timer++;
 		if(beat_10ms_timer == 10) {
 			beat_10ms_timer = 0;
-			printf("time: %b", current_time.tv_nsec);
+      beat_100_ms_timer++;
+			printf("time: %llu", current_time.tv_nsec);
 		}
+    if(beat_100_ms_timer == 10) {
+      beat_100_ms_timer = 0;
+      printf("time: %llu", current_time.tv_nsec);
+      break;
+    }
 	}
+  }
 }
