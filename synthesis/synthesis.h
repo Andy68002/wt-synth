4a311b0c725a8d520fe17da463e0a46cfc7a6957
Andrew McLaurin <andrew@andrewmclaurin.com>
2019-03-25 14:56:53 -0400

    Changed types to explicit types, added to main test

--- synthesis/synthesis.h
+++ synthesis/synthesis.h
@@ -1,3 +1,4 @@
+
 /*Open Source Synthesizer
   Synthesis header file
   Author:
@@ -6,7 +7,19 @@
 #ifndef SYNTHESIS
 #define SYNTHESIS
 
-void DDS(int* note_tuning_words, int* note_attenuation, int* note_enable);
+/* 1/44100s = 22675.737ns */
+#define SAMPLE_RATE 22656
+#define ONE_SECOND_NS 1000000000
+#define NUM_NOTES 6
+#define SAMPLE_WIDTH (16)
+#define WAVETABLE_LENGTH (2^24)
+
+//16 bit unsigned note_tuning_words
+//16 bit unsigned note_attenuation
+//boolean enable/disable
+//32 bit pointer to LUT memory
+void DDS(uint16_t *note_tuning_words, uint16_t *note_attenuation, bool *note_enable, int32_t *LUT);
+void Send_Sample();
 
 
 #endif
