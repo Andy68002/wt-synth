eb2d16822c04378e4d4f56c6c04f00a2e09c5366
Andrew McLaurin <andrew@andrewmclaurin.com>
2019-03-26 13:57:32 -0400

    Added threading. need to go from sample to buffer, not heartbeat

--- synthesis/synthesis.c
+++ synthesis/synthesis.c
@@ -8,62 +8,65 @@
 #include <stdio.h>
 #include <time.h>
 #include "synthesis.h"
-
-void Send_Sample()
-{
-    printf("made it to sending sample");
+int TESTING = 0;
+void Send_Sample(){
 }
 
 void DDS(unsigned short *note_tuning_words,
          unsigned short *note_attenuation,
          char           *note_enable,
-         int            *LUT)
-{
-    // Initialize the heartbeat
+         int            *LUT){
 
-    // get the initial time
 
-    long last_beat;                                 // LONG ns time of last beat
-    long elapsed_time;                              // LONG elapsed time in ns
-    struct timespec time;                           // time struct
-    char calculate_next_sample = FALSE;             // flag to calculate the
-                                                    // next sample
-    unsigned long accumulator[NUM_NOTES];
+	// Initialize the heartbeat
+	long last_beat;                    // LONG ns time of last beat
+	long elapsed_time;                 // LONG elapsed time in ns
+	struct timespec time;              // time struct
+	char calculate_next_sample = FALSE; // flag to calculate the
+	                                    // next sample
+	unsigned long accumulator[NUM_NOTES];
 
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time); // High-res process timer
-    last_beat = time.tv_nsec;                       // ns time of last beat
+	// get the initial time
+	clock_gettime(CLOCK_THREAD_CPUTIME_ID, &time); // High-res process timer
+	last_beat = time.tv_nsec;          // ns time of last beat
 
-    // MAIN LOOP
-    while (1) {
-        // get the current time and calculate the elapsed time
-        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time);
-        elapsed_time = time.tv_nsec - last_beat;
+	// MAIN LOOP
+	while (1) {
+		// get the current time and calculate the elapsed time
+		clock_gettime(CLOCK_THREAD_CPUTIME_ID, &time);
+		elapsed_time = time.tv_nsec - last_beat;
 
-        // Check for 1s rollover
-        if (elapsed_time < 0) {
-            elapsed_time += ONE_SECOND_NS;
-        }
 
-        // check if the elapsed time is >= 1 sample period
-        if (elapsed_time >= SAMPLE_RATE) {
-            // SEND THE SAMPLE
-            // Set flag to calculate next sample
-            Send_Sample();
-            calculate_next_sample = TRUE;
-            break;
-        }
+		// Check for 1s rollover
+		if (elapsed_time < 0) {
+			elapsed_time += ONE_SECOND_NS;
+		}
 
-        // if we need to calculate the next sample, do it
-        if (calculate_next_sample) {
-            // CALCULATE THE NEW SAMPLES
-            // Loop through every notes
-            for (int i = 0; i < NUM_NOTES; i++) {
-                // increment the accumulator and check for overflow
-                accumulator[i] += note_tuning_words[i];
+		// check if the elapsed time is >= 1 sample period
+		if (elapsed_time >= SAMPLE_RATE) {
+			// Set last_beat to new time and check for overflow
+			if ((last_beat += SAMPLE_RATE) < 0) last_beat += ONE_SECOND_NS;
 
-                if (accumulator[i] >
-                    WAVETABLE_LENGTH) accumulator[i] -= WAVETABLE_LENGTH;
-            }
-        }
-    }
+			// SEND THE SAMPLE
+			// Set flag to calculate next sample
+			Send_Sample();
+			printf("%i, %li, \n", TESTING, elapsed_time);
+
+			if (TESTING++ >= 2000) break;
+			calculate_next_sample = TRUE;
+		}
+
+		// if we need to calculate the next sample, do it
+		if (calculate_next_sample) {
+			// CALCULATE THE NEW SAMPLES
+			// Loop through every notes
+			for (int i = 0; i < NUM_NOTES; i++) {
+				// increment the accumulator and check for overflow
+				accumulator[i] += note_tuning_words[i];
+
+				if (accumulator[i] >
+				    WAVETABLE_LENGTH) accumulator[i] -= WAVETABLE_LENGTH;
+			}
+		}
+	}
 } /* DDS */
--- synthesis/synthesisTest.c
+++ synthesis/synthesisTest.c
@@ -4,30 +4,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "synthesis.h"
+#include <pthread.h>
 #define FILE_BUFFER_SIZE 4
+
+// create a struct to store the pointers
+typedef struct {
+	unsigned short *tuning_ptr;
+	unsigned short *attenuate_ptr;
+	char           *enable_ptr;
+	int            *LUT_ptr;
+} passed_variables;
+
+// thread function to break out synthesis to its own thread
+// passed a single struct containing the variables needed
+void* synthesis_thread(void *args) {
+	passed_variables *dds_args = args;
+
+	DDS(dds_args->tuning_ptr,
+	    dds_args->attenuate_ptr,
+	    dds_args->enable_ptr,
+	    dds_args->LUT_ptr);
+}
+
 int main() {
-    // initialize arrays for frequency, attenuation, and enable
-    unsigned short tuning[NUM_NOTES];
-    unsigned short attenuate[NUM_NOTES];
-    char enable[NUM_NOTES];
+	// initialize arrays for frequency, attenuation, and enable
+	unsigned short tuning[NUM_NOTES];
+	unsigned short attenuate[NUM_NOTES];
+	char enable[NUM_NOTES];
 
-    // initialize the memory for the lookup table
-    char *LUT = malloc(SAMPLE_WIDTH * WAVETABLE_LENGTH);
+	// initialize the memory for the lookup table
+	int *LUT = malloc(SAMPLE_WIDTH * WAVETABLE_LENGTH);
 
-    // Reading in the LUT
-    FILE *LUT_File;                     // pointer to file stream
+// initialize the passed variable struct;
+	passed_variables passed_args;
 
-    LUT_File = fopen("test.bin", "rb"); // open file stream
+	passed_args.tuning_ptr    = tuning;
+	passed_args.attenuate_ptr = attenuate;
+	passed_args.enable_ptr    = enable;
+	passed_args.LUT_ptr       = LUT;
 
-    fread(LUT, 1, 48, LUT_File);
-    printf("%X", LUT);
+	// Create a void pointer to the passed variables struct
+	void *args = &passed_args;
 
-    for (int i = 0; i < WAVETABLE_LENGTH; i++) {
-        printf("i: %i, LUT: %c, &LUT: %i\n", i, LUT[i], &LUT[i]);
-    }
+	// Reading in the LUT
+	FILE *LUT_File;                 // pointer to file stream
 
-    // DDS(tuning, attenuate, enable, LUT);
+	LUT_File = fopen("test.bin", "rb"); // open file stream
+//TODO: Add error handling for wave file read
+	fread(LUT, SAMPLE_WIDTH, WAVETABLE_LENGTH, LUT_File);
 
-    free(LUT);
-    return 0;
+	pthread_t thread_id;
+	pthread_create(&thread_id, NULL, synthesis_thread, args);
+//	DDS(tuning, attenuate, enable, LUT);
+	while(1);
+	free(LUT);
+	return 0;
 }
